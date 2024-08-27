fbcd6885f6b2ad9dbd647abd77ecf256e624c112
Zafron047 <117600135+Zafron047@users.noreply.github.com>
2024-08-27 22:16:27 +0600

    Add entities userRole and userStatus

---
+++ lib/src/features/feature_name/domain/entities/user_status.dart
@@ -0,0 +1,13 @@
+class UserStatus {
+  final bool isMember;
+  final bool isInvited;
+  final bool isRequested;
+  final bool isBlocked;
+
+  UserStatus({
+    required this.isMember,
+    required this.isInvited,
+    required this.isRequested,
+    required this.isBlocked,
+  });
+}
