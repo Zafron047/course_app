1c98189d3d8831a5a11b7d624c4899c9be52aed7
Zafron047 <117600135+Zafron047@users.noreply.github.com>
2024-08-29 21:51:11 +0600

    Add business rules for chapter

--- lib/src/features/feature_name/domain/repositories/chapter_repository.dart
+++ lib/src/features/feature_name/domain/repositories/chapter_repository.dart
@@ -1,7 +1,7 @@
 import 'package:course_app/src/features/feature_name/domain/entities/book/chapter_entity.dart';
 
 abstract class ChapterRepository {
-  Future<ChapterEntity> addChapter(ChapterEntity chapter);
-  Future<ChapterEntity> removeChapter(ChapterEntity chapter);
-  Future<ChapterEntity> getChapter(ChapterEntity chapter);
+  Future<void> addChapter(ChapterEntity chapter);
+  Future<void> removeChapter(String chapterId);
+  Future<void> getChapter(String chapterId);
 }
---
+++ lib/src/features/feature_name/domain/usecases/chapter_usecases.dart
@@ -0,0 +1,20 @@
+import 'package:course_app/src/features/feature_name/domain/entities/book/chapter_entity.dart';
+import 'package:course_app/src/features/feature_name/domain/repositories/chapter_repository.dart';
+
+class ChapterUsecases {
+  final ChapterRepository chapterRepository;
+
+  ChapterUsecases(this.chapterRepository);
+
+  Future<void> addChapter(ChapterEntity chapter) async {
+    return await chapterRepository.addChapter(chapter);
+  }
+
+  Future<void> removeChapter(String chapterId) async {
+    return await chapterRepository.removeChapter(chapterId);
+  }
+
+  Future<void> getChapter(String chapterId) async {
+    return await chapterRepository.getChapter(chapterId);
+  }
+}
