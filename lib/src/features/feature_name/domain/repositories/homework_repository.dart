e73aed5110e5bd3a7bd842d95d88c3dc8f1a2bcd
Zafron047 <117600135+Zafron047@users.noreply.github.com>
2024-08-29 21:44:16 +0600

    Add comments to later refactor to SOLID principals

--- lib/src/features/feature_name/domain/repositories/homework_repository.dart
+++ lib/src/features/feature_name/domain/repositories/homework_repository.dart
@@ -1,12 +1,20 @@
-import 'package:course_app/src/features/feature_name/domain/entities/home_work.dart';
+import 'package:course_app/src/features/feature_name/domain/entities/homework/homework_entity.dart';
 
 abstract class HomeWorkRepo {
-  Future<void> submitHomeWork(HomeWorkEntity homeWork);
+  Future<void> submitHomeWork(
+      HomeWorkEntity homeWork); // Submit homework to Homeworks collection
   // Future<void> notifyTeacher(String roomId);
-  Future<void> acceptReview(String homeWorkId, String reviewerId);
-  Future<bool> isReviewer(String homeWorkId, String teacherId);
-  Future<void> finishReview(String homeWorkId, String progressId);
-  Future<void> undoReview(String homeWorkId, String progressId);
+  Future<void> acceptReview(String homeWorkId,
+      String reviewerId); // update reviewerId in Homework Document
+  Future<bool> isReviewer(
+      String homeWorkId, String teacherId); // check if teacher is reviewer
+  Future<void> finishReview(
+      String
+          homeWorkId); // update isReviewed and reviewedDate in Homework Document.
+  Future<void> undoReview(
+      String homeWorkId,
+      String
+          progressId); // update isReviewed reviewedDate in Homework Document and set range as false in UserProgress
   // Future<void> notifyStudent(String homeWorkId);
-  Future<HomeWorkEntity> getHomeWork(String homeWorkId);
+  Future<HomeWorkEntity> getHomeWork(String homeWorkId); // get homework by id
 }
--- lib/src/features/feature_name/domain/repositories/user_progress_repository.dart
+++ lib/src/features/feature_name/domain/repositories/user_progress_repository.dart
@@ -1,13 +1,17 @@
-import 'package:course_app/src/features/feature_name/domain/entities/user_progress.dart';
+import 'package:course_app/src/features/feature_name/domain/entities/homework/homework_entity.dart';
+import 'package:course_app/src/features/feature_name/domain/entities/user/user_progress_entity.dart';
 
 abstract class UserProgressRepository {
-  Future<UserProgressEntity> updateProgress(
-      String homeWorkId, String userProgressId);
+  Future<void> updateProgress(String studentId, String bookId, String chapterId,
+      Map<int, bool> sectionRange); // Update userProgress as per review
   Future<UserProgressEntity> undoUpdate(
       String homeWorkId, String userProgressId);
-  Future<bool> hasBook(String bookId);
-  Future<void> addBook(String bookId);
+  Future<bool> hasBook(String bookId); // check if userProgress has the book
+  Future<void> addBook(String bookId); // add book to userProgress
   Future<void> addChapters(String bookId);
   Future<void> addSections(String bookId);
+  Future<bool> isRangeValid(
+      HomeWorkEntity
+          homeWork); // check if user has the book? add book. Then check if the range is valid?
   Future<UserProgressEntity> getUserProgress(String userId, String bookId);
 }
